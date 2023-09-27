e2cbfd5e42709720d64f16b8294c105021126626
GabrielPuddu2106 <142625919+GabrielPuddu2106@users.noreply.github.com>
2023-09-26 18:10:29 -0600

    commit23

---
+++ Ciudades.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LibroCalificaciones{
+    public static void main(String args[]){
+        Scanner lector = new Scanner(System.in);
+
+
+        System.out.println("Ingrese la cantidad de estudiantes");
+         int CantEstudiantes = lector.nextInt();
+
+        System.out.println("Ingrese la cantidad de notas ");
+         int CantNotas = lector.nextInt();
+        int[][] matrizA = new int [CantEstudiantes][CantNotas];
+
+        //INGRESO DE LOS ELEMENTOS DE LA MATRIZ
+
+        System.out.println("Ingrese los paises ");
+        for(int i = 0; i<matrizA.length; i++){//para filas
+            for(int j =0; j<matrizA[length].length; j++){//para las columnas
+                System.out.println("MatriA[" + i + "] [" + j + "] :");
+                matrizA[]
+
+            }
+
+        }
+        
+    }
+}
